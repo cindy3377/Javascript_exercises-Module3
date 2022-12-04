483287137ad7800de3071fcb8fd9949b4cbbdb80
cindy3377 <92247502+cindy3377@users.noreply.github.com>
2022-12-04 12:45:32 +0200

    upload ex8

--- t8/8.js
+++ t8/8.js
@@ -4,4 +4,18 @@ There are two input fields where user enters numbers. Based on the drop-down lis
 Use the value attribute of <option> elements to decide which operation the calculator needs to do. Example.
 Show the result in <p id="result"> when the button is clicked.
  */
+function calculate() {
+    let valueInput1 = parseInt(document.getElementById('num1').value)
+    let valueInput2 = parseInt(document.getElementById('num2').value)
+    let selectedOption = document.getElementById('operation').value
+    if(selectedOption == 'add') return valueInput1 + valueInput2
+    else if (selectedOption == 'sub') return valueInput1 - valueInput2
+    else if (selectedOption == 'multi') return valueInput1 * valueInput2
+    else return valueInput1 / valueInput2
+}
 
+function onClickCalculate() {
+    let calculatedValue = calculate()
+    let result = document.getElementById('result')
+    result.innerHTML = calculatedValue
+}
